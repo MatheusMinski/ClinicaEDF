0826592b59ea2929c570d9da994473123cb21400
Matheus Minski <minskisantos@hotmail.com>
2019-11-10 16:10:21 -0300

    testeworkspace

--- app/Http/Controllers/LoginProfessorController.php
+++ app/Http/Controllers/LoginProfessorController.php
@@ -19,11 +19,23 @@ class LoginProfessorController extends Controller
     public function entrar(Request $req){
 
         $dados = $req-> all();
-        if(Auth::attempt(['email'=>$dados['email'],'password'=>$dados['password']])){
-            return redirect()->route('home');
+
+
+        $prof = User::where('email', $dados['email'])->first();
+
+        if($prof != null){
+            if($prof->ativo){
+                if(Auth::attempt(['email'=>$dados['email'],'password'=>$dados['password']])) {
+                    return redirect()->route('home');
+                }
+                    return redirect()->back()->withErrors(['Login ou senha inválidos']);
+            }else{
+                return redirect()->back()->withErrors(['O professor está inativo!']);
+            }
+        }else{
+            return redirect()->back()->withErrors(['O e-mail não está cadastrado']);
         }
 
-        return redirect()->route('login');
     }
 
     public function sair(){
