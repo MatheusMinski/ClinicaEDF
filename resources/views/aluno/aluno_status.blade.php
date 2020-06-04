801161b65535a0ef96ba76e77a3ed9311660db89
Lucas Vieira <lucasvp611@hotmail.com>
2020-06-04 14:48:07 -0300

    rotas para botao e datas js

--- resources/views/aluno/aluno_status.blade.php
+++ resources/views/aluno/aluno_status.blade.php
@@ -19,32 +19,37 @@
                         <td>Sem estatus</td>
                     </tr>
                    <tr>
-                        <td><a href="aluno_cadastro_anamnese.blade.php" class="btn blue">Anamnese</a></td>
+                        <td><a href="{{route('aluno.cadastro.anamnese')}}" class="btn blue">Anamnese</a></td>
                         <td>{{ $anamnese }}</td>
                     </tr>
                     <tr>
-                        <td><a href="aluno_cadastro_avaliacaoFuncional.blade.php" class="btn blue">Avaliação Funcional</a></td>
+                        <td><a href="{{route('aluno.cadastro.avaliacao')}}" class="btn blue">Avaliação Funcional</a></td>
                         <td>{{ $avaliacaoFuncional }}</td>
                     </tr>
                     <tr>
-                        <td><a href="aluno_cadastro_qualidadeVida.blade.php" class="btn blue">Qualidade de Vida</a></td>
+                        <td><a href="{{route('aluno.cadastro.qualidadeVida')}}" class="btn blue">Qualidade de Vida</a></td>
                         <td>{{ $qualidadeDeVidas }}</td>
                     </tr>
                     <tr>
-                        <td><a href="aluno_cadastro_medicamentos.blade.php" class="btn blue">Uso de Medicamentos Contínuos</a></td>
+                        <td><a href="{{route('aluno.cadastro.medicamentos')}}" class="btn blue">Uso de Medicamentos Contínuos</a></td>
                         <td>{{ $usoMedicamentosContinuos }}</td>
                     </tr>
                     <tr>
-                        <td><a href="aluno_cadastro_perfilBioquimico.blade.php" class="btn blue">Perfil Bioquímico</a></td>
+                        <td><a href="{{route('aluno.cadastro.perfilBioquimico')}}" class="btn blue">Perfil Bioquímico</a></td>
                         <td>{{ $perfilBioquimico }}</td>
                     </tr>
                     <tr>
-                        <td><a href="aluno_cadastro_exames.blade.php" class="btn blue">Exames Adicionais (Se houver)</a></td>
+                        <td><a href="{{route('aluno.cadastro.exames')}}" class="btn blue">Exames Adicionais (Se houver)</a></td>
                         <td>{{ $examesAdicionais }}</td>
                     </tr>
                     <tr>
-                        <td><a href="aluno_cadastro_consultas.blade.php" class="btn blue">Quantas Consultas (Se houver)</a></td>
+                        <td><a href="{{route('aluno.cadastro.consultas')}}" class="btn blue">Quantas Consultas (Se houver)</a></td>
                         <td>{{ $quantasConsultas }}</td>
+                    </tr>
+
+                    <tr>
+                        <td><a href="{{route('aluno.cadastro.emergencia')}}" class="btn blue">Contato de Emergência</a></td>
+                        <!--<td>  //$contatoEmergencia </td> -->
                     </tr>
 
                 </tbody>
