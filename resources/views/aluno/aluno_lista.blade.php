02bf0a5d1e157bc53df01c2cf9d5c1b8843b1511
Matheus Minski dos Santos <minskisantos@hotmail.com>
2021-06-30 18:33:08 -0300

    erro em uma view arrumado

--- resources/views/aluno/aluno_lista.blade.php
+++ resources/views/aluno/aluno_lista.blade.php
@@ -21,7 +21,6 @@
                     <th>Nome</th>
                     <th>Dados Pessoais</th>
                     <th>Treinamentos</th>
-                    @if(Auth()->user()->type == is)
 
                     <th><a href="{{route('alunos.exportar')}}" class="btn red">Exportar</a></th>
                 </tr>
